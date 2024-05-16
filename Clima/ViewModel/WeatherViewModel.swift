b67aeb4f4eb9c578a786545412e2af8a92aff77d
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2024-05-16 11:34:14 +0300

    Fix: First time location bug

--- Clima/ViewModel/WeatherViewModel.swift
+++ Clima/ViewModel/WeatherViewModel.swift
@@ -19,21 +19,25 @@ class WeatherViewModel: ObservableObject {
     
     func fetchWeatherResponse() {
         guard let currentLocation = locationManager.location else {
-            print("Location not available yet.")
+            print("Location not available yet. Retrying in 1 second...")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+                self?.fetchWeatherResponse()
+            }
             return
         }
-        
+
         NetworkService.fetchWeather(with: "\(currentLocation.latitude)", and: "\(currentLocation.longitude)") { [weak self] result in
             switch result {
             case .success(let success):
                 DispatchQueue.main.async {
                     self?.weatherResponse = success
-//                    print(success)
                 }
             case .failure(let failure):
                 print(failure.localizedDescription)
             }
         }
     }
+
 }
 
